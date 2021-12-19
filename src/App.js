0df2d94bcc07651cc169f6f595fcf6168b1c6878
stefantanasa <liketanasa@gmail.com>
2021-12-19 10:16:08 +0000

    start

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Container, Row } from 'react-bootstrap';
+import './App.css';
+import `bootstrap/dist/css/bootstrap.mini.css`
+
+function App() {
+  return (
+    
+<Container>
+<Row>
+<Col md={4}>
+<h1>Lessons React</h1>
+
+</Col>
+
+</Row>
+</Container>
+  );
+}
+
+export default App;
