4f6a4fcbb910519b7433a761cdf5d134be9e4135
stefantanasa <liketanasa@gmail.com>
2021-12-20 11:27:52 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Counter from "./components/counter";
 
 function App() {
   return (
     <Container>
       <Row>
         <Col md={4}>
-          <h1>Lessons React</h1>
+          <h1>This is text is from the app</h1>
+          <Counter />
         </Col>
       </Row>
     </Container>
---
+++ src/components/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() { 
+        return <h2>This text is from the counter component</h2>
+    }
+}
+ 
+export default Counter;
